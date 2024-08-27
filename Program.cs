8d3fe246f3e06b87d564629f952f7daec4884a3f
boombuster2000 <chanuka.williams@outlook.com>
2024-08-27 18:54:18 +0100

    Created function to validate input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace PizzaOrderer
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PizzaOrderer
 {
     class Program
     {
@@ -8,11 +10,29 @@
             System.Console.WriteLine("Menu");
             System.Console.WriteLine("1) Medium Pizza \t £2");
             System.Console.WriteLine("2) Large Pizza \t\t £3");
+            System.Console.WriteLine("x) Exit");
+        }
+
+        static string getOption(string[] validOptions)
+        {
+            while (true)
+            {
+                System.Console.Write(">> ");
+                string? userOption = System.Console.ReadLine();
+                if (String.IsNullOrEmpty(userOption)) continue;
+                userOption = userOption.ToLower();
+
+                bool isValidOption = false;
+                foreach (string option in validOptions) if (option.Equals(userOption)) isValidOption = true;
+                if (!isValidOption) continue;
+                return userOption;
+            }
         }
 
         static void Main()
         {
             PrintMenu();
+            string userOption = getOption(["1", "2", "x"]);
         }
     }
 }
