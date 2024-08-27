7fa1a078d01d5599491c5b18c8ade6bafc509299
boombuster2000 <chanuka.williams@outlook.com>
2024-08-27 18:21:45 +0100

    Prints Menu

--- Program.cs
+++ Program.cs
@@ -2,9 +2,17 @@
 {
     class Program
     {
+
+        static void PrintMenu()
+        {
+            System.Console.WriteLine("Menu");
+            System.Console.WriteLine("1) Medium Pizza \t £2");
+            System.Console.WriteLine("2) Large Pizza \t\t £3");
+        }
+
         static void Main()
         {
-
+            PrintMenu();
         }
     }
 }
