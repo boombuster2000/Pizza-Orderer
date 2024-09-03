25722406254a108f45e77efc9eee95ab4ab8339c
boombuster2000 <chanuka.williams@outlook.com>
2024-09-03 17:06:04 +0100

    Removed unnecesary imports

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-
-namespace PizzaOrderer
+﻿namespace PizzaOrderer
 {
     class Program
     {
